ce2b555c7bd7bd23923be6b56c5eac1f783cf2da
Praveen GN <praveen@insuredmine.com>
2021-11-19 21:56:24 +0530

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytestapp4a3015da": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+// import { withAuthenticator } from "aws-amplify-react";
+import { Auth } from 'aws-amplify';
+
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(awsConfig);
+
+const onSignOutClick = async () => {
+  await Auth.signOut()
+      .then(data => {
+        console.log('success',data);
+        window.location.reload(false);
+      })
+      .catch(err => console.log('error',err));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> Hola, sign in success</h2>
+        <button onClick={onSignOutClick}>
+          Log out
+        </button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
